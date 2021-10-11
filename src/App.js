daceb4ad9631b226067aa52a585e32ee4c257292
Mariko971 <slimmack@gmail.com>
2021-10-11 16:06:25 +0400

    created new-feedback component in the pages folder to facilitate a feature for creating feedbacks in the app, also created a route for it in the app.js file.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router';
 
 import './App.css';
 import SuggestionsPage from './components/pages/suggestions-page/suggestions-page.component';
+import NewFeedbackForm from './components/pages/new-feedback/new-feedback.component';
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={SuggestionsPage}/>
+      <Route exact path='/new-feedback' component = {NewFeedbackForm}/>
     </div>
   );
 };
