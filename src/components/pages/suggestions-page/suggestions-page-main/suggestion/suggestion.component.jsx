9240ea5828bf1b60456fbefeff7413058a3ff580
Mariko971 <slimmack@gmail.com>
2021-10-11 15:06:55 +0400

    created assets folder in src containing mobile & desktop background imgs for the header component.  Added suggestions-page folder in components folder under pages folder. included within is the suggestion-aside and suggestion-main components of the main page (suggestions-page);

---
+++ src/components/pages/suggestions-page/suggestions-page-main/suggestion/suggestion.component.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+
+import './suggestion.style.scss';
+import { upvoterAction } from '../../../../../redux/actions/upvote.action';
+
+export const commentsCount = (comments)=>{
+    let count = comments.length;
+    if(comments.length>0){
+        comments.map((comment)=>
+            Object.keys(comment).includes('replies') ?                 
+             count += comment.replies.length
+             : count
+            );
+        return count;
+    } else return count;
+}
+
+const Suggestion = (props)=>{
+    const {id, title, description, category, upvotes, comments, appDataReducer} = props;
+    
+    return(
+        <div className="suggestion-container" >
+            <div className="suggestion-upvote" onClick={()=>appDataReducer(id)}>
+                <img  src="/assets/shared/icon-arrow-up.svg" alt="up arrow"/>
+                <p className="suggestion-upvote-votes">{upvotes}</p> 
+            </div>
+            <div className="suggestion-main" onClick={()=>props.history.push(`/feedback/${id}`)}>
+                <h3 className='suggestion-main-title'>
+                    {title}
+                </h3>
+                <p className='suggestion-main-txt'>
+                    {description}
+                </p>
+                <p className='suggestion-main-topic body-3'>
+                    {category}
+                </p>
+            </div>
+            <div className="suggestion-comments">
+                <img className="suggestion-comments-icon" src="/assets/shared/icon-comments.svg" alt="comments" />
+                <p className="suggestion-comments-count">{commentsCount(comments)}</p>
+            </div>
+        </div>
+    )
+}
+const mapDispatchToProps = dispatch =>({
+appDataReducer: id => dispatch(upvoterAction(id))
+});
+
+export default connect(null, mapDispatchToProps)(withRouter(Suggestion));
